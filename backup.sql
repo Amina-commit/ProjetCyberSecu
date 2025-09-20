6c4e9eaf54d4b8464a7a8305e1643b80ecaa258c
Aminata <aminatacisse41h@gmail.com>
2025-09-20 21:08:40 +0200

    projetCyberSecu

---
+++ backup.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.19  Distrib 10.3.39-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: bdmamie
+-- ------------------------------------------------------
+-- Server version	10.3.39-MariaDB-0+deb10u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cafe`
+--
+
+DROP TABLE IF EXISTS `cafe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cafe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prix` varchar(30) NOT NULL,
+  `pays` varchar(30) NOT NULL,
+  `description` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cafe`
+--
+
+LOCK TABLES `cafe` WRITE;
+/*!40000 ALTER TABLE `cafe` DISABLE KEYS */;
+INSERT INTO `cafe` VALUES (1,'amina','10','france','bonjour'),(2,'Aminta','32','brasil','bonjour');
+/*!40000 ALTER TABLE `cafe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctrine_migration_versions`
+--
+
+LOCK TABLES `doctrine_migration_versions` WRITE;
+/*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20250311131312','2025-03-11 13:15:20',54),('DoctrineMigrations\\Version20250401111129','2025-04-01 11:11:45',102),('DoctrineMigrations\\Version20250401112851','2025-04-01 11:28:58',12),('DoctrineMigrations\\Version20250401130123','2025-04-01 13:01:27',14);
+/*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) NOT NULL,
+  `roles` longtext NOT NULL COMMENT '(DC2Type:json)',
+  `password` varchar(255) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'aminatacisse41h@gmail.com','[\"ROLE_ADMIN\"]','$2y$13$TpwzSrFEkYaaFPxGBhx94.HLicGDA9.JR6Wx9zkijgB.iL3W.5AhC','','','0000-00-00',NULL),(2,'aminanuage@gmail.com','[\"ROLE_MOD\"]','$2y$13$oVuaxyjVolyPJgKMs.r/6efv6a4cLoq0kVJ2fGVUy4UdUgXvL5zmu','amina','cisse','2020-01-01','chat.jpeg'),(3,'julien@gmail.com','[]','$2y$13$5JMI7T5pioPjrVGMlWHn8u5iPLvbqAHEK00fwKCKugbwGyWQt9zUO','ju','ki','2020-01-01',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-15 16:19:47
---
+++ src/Command/AppCommandDumpDatabaseCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class DumpDatabaseCommand extends Command
+{
+    protected static $defaultName = 'app:dump-database';
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Dump the database to a SQL file')
+            ->addOption('output', null, InputOption::VALUE_REQUIRED, 'Output file path', 'backup.sql');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $outputFile = $input->getOption('output');
+        $connection = $this->entityManager->getConnection();
+        $params = $connection->getParams();
+
+        $command = sprintf(
+            'mysqldump -u %s -p%s %s > %s',
+            $params['user'],
+            $params['password'],
+            $params['bdcyber'],
+            $outputFile
+        );
+
+        system($command, $returnVar);
+
+        if ($returnVar !== 0) {
+            $output->writeln('<error>Failed to dump the database.</error>');
+            return Command::FAILURE;
+        }
+
+        $output->writeln('<info>Database dumped successfully to ' . $outputFile . '</info>');
+        return Command::SUCCESS;
+    }
+}
